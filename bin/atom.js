97db0a806a0b3737ccf3e75c625d708b027c8fed
Ricardo Nogueira <seuze.rick@gmail.com>
2021-04-15 11:13:48 -0300

    :sparkles: it adds the capability of specify the folder name :fire: it removes trash

--- bin/atom.js
+++ bin/atom.js
@@ -1,14 +1,22 @@
 #! /usr/bin/env node
 const Fs = require("fs");
-// const shell = require("shelljs");
 const src = "./src";
 const args = process.argv.slice(2);
-const name = args[0];
+const path = args[0].split("/");
+let name = "";
+let folderName = "";
+if (path.length > 1) {
+  name = path[1];
+  folderName = path[0];
+} else {
+  name = args[0];
+  folderName = args[0];
+}
 const fileName = name.charAt(0).toUpperCase() + name.slice(1);
 
 const createAtom = () => {
   Fs.writeFile(
-    `${src}/atoms/${name}/${fileName}.js`,
+    `${src}/atoms/${folderName}/${fileName}.js`,
     `import React from "react";
 import { View, Text } from "react-native";
 
@@ -28,7 +36,7 @@ export { ${fileName} };`,
   );
 
   Fs.appendFile(
-    `${src}/atoms/${name}/index.js`,
+    `${src}/atoms/${folderName}/index.js`,
     `export * from './${fileName}'\n`,
     function (err) {
       if (err) throw err;
@@ -40,7 +48,7 @@ export { ${fileName} };`,
 // src folder and atoms folder
 if (!Fs.existsSync(src)) {
   Fs.mkdirSync(src);
-  console.log("src folder created");
+  console.log("Src folder: created");
 }
 if (!Fs.existsSync(`${src}/atoms`)) {
   Fs.mkdirSync(`${src}/atoms`);
@@ -49,32 +57,26 @@ if (!Fs.existsSync(`${src}/atoms`)) {
     "//exports all atoms\n",
     function (err) {
       if (err) throw err;
-      console.log("atoms folder created");
+      console.log("Atoms folder: created");
     }
   );
 }
 
-Fs.appendFile(
-  `${src}/atoms/index.js`,
-  `export * from './${name}'\n`,
-  function (err) {
-    if (err) throw err;
-    console.log("Updated index.js file!");
-  }
-);
-
-if (!Fs.existsSync(`${src}/atoms/${name}`)) {
-  Fs.mkdirSync(`${src}/atoms/${name}`);
+if (!Fs.existsSync(`${src}/atoms/${folderName}`)) {
+  Fs.mkdirSync(`${src}/atoms/${folderName}`);
   createAtom();
-} else if (
-  !Fs.existsSync(
-    `${src}/${args[0]}/${args[0].charAt(0).toUpperCase() + args[0].slice(1)}.js`
-  )
-) {
+
+  //it adds an export statment in index.js file from atoms folder
+  Fs.appendFile(
+    `${src}/atoms/index.js`,
+    `export * from './${folderName}'\n`,
+    function (err) {
+      if (err) throw err;
+      console.log("Updated index.js file!");
+    }
+  );
+} else if (!Fs.existsSync(`${src}/atoms/${folderName}/${fileName}.js`)) {
   createAtom();
 } else {
   throw new Error("Átomo já existe");
 }
-// shell.exec("mkdir ./src");
-// shell.exec("mkdir ./src/atoms");
-// shell.exec("touch ./src/atoms/teste.js");
